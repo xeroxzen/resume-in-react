e2fd11c4fe4f0f2a727676d9be6f939836ac8ce1
Google Jr <andilembele020@gmail.com>
2022-04-20 23:33:42 +0200

    Coding on a big monitor is awesome

--- src/Components/About.js
+++ src/Components/About.js
@@ -8,6 +8,7 @@ class About extends Component {
     const name = this.props.data.name;
     const profilepic = "images/" + this.props.data.image;
     const bio = this.props.data.bio;
+    const githubBio = this.props.data.githubBio;
     // const street = this.props.data.address.street;
     const city = this.props.data.address.city;
     const country = this.props.data.address.country;
