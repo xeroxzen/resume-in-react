1375243e4aa3a0a358fa8722450fb374c112fe03
Google Jr <andilembele020@gmail.com>
2022-04-20 17:00:51 +0200

    Sokusharp

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -15,6 +15,13 @@ class Footer extends Component {
       );
     });
 
+    const current = new Date();
+    const date = `${current.getDate()}/${current.getMonth()+1}/${current.getFullYear()}`;
+    const year = current.getFullYear();
+    // if minutes is less than 10 minutes add 0
+
+    const time = `${current.getHours()}:${current.getMinutes()}`
+
     return (
       <footer>
         <div className="row">
@@ -23,13 +30,9 @@ class Footer extends Component {
               <ul className="social-links">{networks}</ul>
 
               <ul className="copyright">
-                <li>&copy; Copyright 2021 Nordic Giant</li>
-                <li>
-                  Design by{" "}
-                  <a title="Styleshout" href="http://www.styleshout.com/">
-                    Styleshout
-                  </a>
-                </li>
+                <li>&copy; Copyright {year} Andile Jaden Mbele</li>
+                <p>Current time: {time}</p>
+
               </ul>
             </div>
           </Fade>
