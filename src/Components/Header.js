a7e4edd3de9d88b44732e88b0d3a37c936055655
Google Jr <andilembele020@gmail.com>
2022-04-22 15:48:21 +0200

    some changes

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -13,7 +13,7 @@ class Header extends Component {
 
     return (
       <header id="home">
-        <ParticlesBg type="circle" bg={true} />
+        <ParticlesBg type="lines" bg={true} />
 
         <nav id="nav-wrap">
           <a className="mobile-btn" href="#nav-wrap" title="Show navigation">
