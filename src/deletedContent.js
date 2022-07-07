a001e6879761f765023c2651ca0fc85bb5d6bd88
Google Jr <andilembele020@gmail.com>
2022-07-07 10:29:43 +0200

    removed contact section

--- src/deletedContent.js
+++ src/deletedContent.js
@@ -24,3 +24,77 @@
 //     </b>
 //   </li>
 // </ul>
+
+// Contact page
+<Slide left duration={1000}>
+  <div className="eight columns">
+    <form action="" method="post" id="contactForm" name="contactForm">
+      <fieldset>
+        <div>
+          <label htmlFor="contactName">
+            Name <span className="required">*</span>
+          </label>
+          <input
+            type="text"
+            defaultValue=""
+            size="35"
+            id="contactName"
+            name="contactName"
+            onChange={this.handleChange}
+          />
+        </div>
+
+        <div>
+          <label htmlFor="contactEmail">
+            Email <span className="required">*</span>
+          </label>
+          <input
+            type="text"
+            defaultValue=""
+            size="35"
+            id="contactEmail"
+            name="contactEmail"
+            onChange={this.handleChange}
+          />
+        </div>
+
+        <div>
+          <label htmlFor="contactSubject">Subject</label>
+          <input
+            type="text"
+            defaultValue=""
+            size="35"
+            id="contactSubject"
+            name="contactSubject"
+            onChange={this.handleChange}
+          />
+        </div>
+
+        <div>
+          <label htmlFor="contactMessage">
+            Message <span className="required">*</span>
+          </label>
+          <textarea
+            cols="50"
+            rows="15"
+            id="contactMessage"
+            name="contactMessage"
+          ></textarea>
+        </div>
+
+        <div>
+          <button className="submit">Submit</button>
+          <span id="image-loader">
+            <img alt="" src="images/loader.gif" />
+          </span>
+        </div>
+      </fieldset>
+    </form>
+
+    <div id="message-warning"> Error boy</div>
+    <div id="message-success">
+      <i className="fa fa-check"></i>Your message was sent, thank you!
+      <br />
+    </div>
+  </div>
+</Slide>;
