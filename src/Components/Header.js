020ab9778ea227e8c57545a744bee73e9349e23f
Google Jr <andilembele020@gmail.com>
2022-07-17 11:43:05 +0200

    target _blank

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -67,7 +67,7 @@ class Header extends Component {
               <h1 className="responsive-headline">{name}</h1>
             </Fade>
             <Fade bottom duration={1200}>
-              <h3>{description}.</h3>
+              <h3>{description}</h3>
             </Fade>
             <hr />
             <Fade bottom duration={2000}>
