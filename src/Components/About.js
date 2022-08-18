b36e5ab9ba31ad46a576809038fc6cd8bb80266e
Google Jr <andilembele020@gmail.com>
2022-08-19 00:01:33 +0200

    __blank

--- src/Components/About.js
+++ src/Components/About.js
@@ -49,7 +49,12 @@ class About extends Component {
                 </div>
                 <div className="columns download">
                   <p>
-                    <a href={resumeDownload} className="button">
+                    <a
+                      href={resumeDownload}
+                      className="button"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
                       <i className="fa fa-download"></i>Download Resume
                     </a>
                   </p>
